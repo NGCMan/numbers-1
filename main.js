ed34b106a58430d56f3ea3230427380bff51ccde
Katakana1 <40336934+Katakana1@users.noreply.github.com>
2019-05-29 19:33:26 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,11 @@
+number = {
+  mag: 0,
+  tlayer: 0;
+};
+setInterval(function() {
+  number.mag++;
+  updt();
+}, 50);
+function updt() {
+  document.getElementById("number").innerHTML = number.mag;
+};
