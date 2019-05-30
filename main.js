18175cbc71b451b09bc2b4627eee5189dc7bfc2f
Katakana1 <40336934+Katakana1@users.noreply.github.com>
2019-05-30 17:22:56 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
-number = {
+var number = {
   mag: 0,
   tlayer: 0
 };
+var frs = 0;
 function commaNumber(x) {
     x = x.toString();
     var pattern = /(-?\d+)(\d{3})/;
@@ -12,11 +13,21 @@ function commaNumber(x) {
 setInterval(function() {
   if(number.mag < 2000 && number.tlayer == 0){
     number.mag++;
+  } else if(number.tlayer == 0) {
+    number.mag*=Math.pow(10,1/4620);
   } else {
-    number.mag*=Math.pow(1.01,0.05);
+    number.mag+=1/4620;
   }
   updt();
 }, 50);
 function updt() {
-  document.getElementById("number").innerHTML = commaNumber(Math.round(number.mag));
+  if(number.mag >= 1e10){
+    number.mag = Math.log(number.mag);
+    number.tlayer++;
+  }
+  if(number.tlayer == 0){
+    document.getElementById("number").innerHTML = commaNumber(Math.round(number.mag));
+  } else {
+    document.getElementById("number").innerHTML = Math.pow(10,number.mag%1).toFixed(6) + " × 10<sup>" + Math.floor(number.mag) + "</sup>";
+  }
 };
