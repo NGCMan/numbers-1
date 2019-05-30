f00f233e669b99e83e833eef98df2e5cad82d6a7
Katakana1 <40336934+Katakana1@users.noreply.github.com>
2019-05-29 20:50:47 -0400

    Update main.js

--- main.js
+++ main.js
@@ -2,10 +2,17 @@ number = {
   mag: 0,
   tlayer: 0
 };
+function commaNumber(x) {
+    x = x.toString();
+    var pattern = /(-?\d+)(\d{3})/;
+    while (pattern.test(x))
+        x = x.replace(pattern, "$1,$2");
+    return x;
+};
 setInterval(function() {
   number.mag++;
   updt();
 }, 50);
 function updt() {
-  document.getElementById("number").innerHTML = number.mag;
+  document.getElementById("number").innerHTML = commaNumber(number.mag);
 };
