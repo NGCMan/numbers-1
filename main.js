3898c020774cf628c58d19107d699cd3ec2dcfb5
Katakana1 <40336934+Katakana1@users.noreply.github.com>
2019-06-01 08:00:46 -0400

    Update main.js

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ var number = {
   mag: 0,
   tlayer: 0
 };
+var mlt = 1;
+var dnm = 4620;
 var frs = 0;
 function commaNumber(x) {
     x = x.toString();
@@ -16,8 +18,8 @@ setInterval(function() {
   } else if(number.tlayer == 0) {
     number.mag*=Math.pow(10,1/4620);
   } else {
-    number.mag*=1+(1/(1/Math.pow(25.66666,frs/36000)*46200));
-    if(frs <= 36000){
+    number.mag*=1+(1/(1/Math.pow((dnm*10)/(2000/mlt),frs/(36000/mlt))*(dnm*10)));
+    if(frs <= 36000/mlt){
       frs++;
     }
   }
@@ -27,10 +29,15 @@ function updt() {
   if(number.mag >= 1e10){
     number.mag = Math.log10(number.mag);
     number.tlayer++;
+    frs = 0;
   }
+  mlt = 1+(number.tlayer/5);
+  dnm = 1/log10(prc)*20;
   if(number.tlayer == 0){
     document.getElementById("number").innerHTML = commaNumber(Math.round(number.mag));
-  } else {
+  } else if(number.tlayer == 1) {
     document.getElementById("number").innerHTML = Math.pow(10,number.mag%1).toFixed(6) + " × 10<sup>" + commaNumber(Math.floor(number.mag)) + "</sup>";
+  } else if(number.tlayer == 2) {
+    document.getElementById("number").innerHTML = "10<sup>" + Math.pow(10,number.mag%1).toFixed(6) + " × 10<sup>" + commaNumber(Math.floor(number.mag)) + "</sup></sup>";
   }
 };
