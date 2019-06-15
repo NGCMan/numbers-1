ecbb1955dbb63e50d091c0d803c380215378812c
Katakana1 <40336934+Katakana1@users.noreply.github.com>
2019-06-15 13:56:54 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 var number = {
   mag: 0,
   tlayer: 0,
-  pelayer: 0
+  pelayer: 0,
+  hxlayer: 0
 };
 var mlt = 1;
 var dnm = 4620;
@@ -16,7 +17,7 @@ function commaNumber(x) {
     return x;
 };
 setInterval(function() {
-  if(number.mag < ((1/Math.pow(1.21,number.pelayer))*2000) && number.tlayer == 0){
+  if(number.mag < ((1/mlt*2000) && number.tlayer == 0){
     number.mag++;
   } else if(number.tlayer == 0) {
     number.mag*=Math.pow(10,1/dnm);
@@ -43,30 +44,46 @@ function updt() {
       nlock++;
     }
   }
+  if(number.mag == NaN){
+    number.mag = number.pelayer;
+    number.hxlayer++;
+    number.pelayer = 0;
+    number.tlayer = 0;
+    frs = 0;
+    if(nlock == 1){
+      nlock++;
+    }
+  }
   if(nlock == 0){
     mlt = Math.pow(1.1,number.tlayer);
   } else if(nlock == 1){
     mlt = Math.pow(Math.pow(1.21,number.pelayer),number.tlayer+1);
+  } else if(nlock == 2){
+    mlt = Math.pow(Math.pow(Math.pow(1.4641,number.hxlayer),number.pelayer+1),number.tlayer+1);
   }
   prc = 1+(mlt/100);
   dnm = 1/Math.log10(prc)*20;
-  if(number.tlayer == 0 && number.pelayer == 0){
+  if(number.tlayer == 0 && number.pelayer == 0 && number.hxlayer == 0){
     document.getElementById("number").innerHTML = commaNumber(Math.round(number.mag));
-  } else if(number.tlayer == 1 && number.pelayer == 0) {
+  } else if(number.tlayer == 1 && number.pelayer == 0 && number.hxlayer == 0) {
     document.getElementById("number").innerHTML = Math.pow(10,number.mag%1).toFixed(6) + " × 10<sup>" + commaNumber(Math.floor(number.mag)) + "</sup>";
-  } else if(number.tlayer == 2 && number.pelayer == 0) {
+  } else if(number.tlayer == 2 && number.pelayer == 0 && number.hxlayer == 0) {
     document.getElementById("number").innerHTML = "10<sup>" + Math.pow(10,number.mag%1).toFixed(6) + " × 10<sup>" + commaNumber(Math.floor(number.mag)) + "</sup></sup>";
-  } else if(number.tlayer == 3 && number.pelayer == 0) {
+  } else if(number.tlayer == 3 && number.pelayer == 0 && number.hxlayer == 0) {
     document.getElementById("number").innerHTML = "10<sup>10<sup>" + Math.pow(10,number.mag%1).toFixed(6) + " × 10<sup>" + commaNumber(Math.floor(number.mag)) + "</sup></sup></sup>";
-  } else if(number.tlayer == 4 && number.pelayer == 0) {
+  } else if(number.tlayer == 4 && number.pelayer == 0 && number.hxlayer == 0) {
     document.getElementById("number").innerHTML = "10<sup>10<sup>10<sup>" + Math.pow(10,number.mag%1).toFixed(6) + " × 10<sup>" + commaNumber(Math.floor(number.mag)) + "</sup></sup></sup></sup>";
-  } else if(number.tlayer == 5 && number.pelayer == 0) {
+  } else if(number.tlayer == 5 && number.pelayer == 0 && number.hxlayer == 0) {
     document.getElementById("number").innerHTML = "10<sup>10<sup>10<sup>10<sup>" + Math.pow(10,number.mag%1).toFixed(6) + " × 10<sup>" + commaNumber(Math.floor(number.mag)) + "</sup></sup></sup></sup></sup>";
-  } else if(number.pelayer == 0) {
+  } else if(number.pelayer == 0 && number.hxlayer == 0) {
     document.getElementById("number").innerHTML = "E" + Math.pow(10,number.mag%1).toFixed(4) + "E" + commaNumber(Math.floor(number.mag)) + "#" + (number.tlayer-1);
-  } else if(number.pelayer > 0 && number.tlayer == 0) {
+  } else if(number.pelayer > 0 && number.tlayer == 0 && number.hxlayer == 0) {
     document.getElementById("number").innerHTML = "E" + commaNumber(Math.round(number.mag)) + "#0#" + (number.pelayer+1);
-  } else if(number.pelayer > 0 && number.tlayer > 0) {
+  } else if(number.pelayer > 0 && number.tlayer > 0 && number.hxlayer == 0) {
     document.getElementById("number").innerHTML = "E" + Math.pow(10,number.mag%1).toFixed(4) + "E" + commaNumber(Math.floor(number.mag)) + "#" + (number.tlayer-1) + "#" + (number.pelayer+1);
+  } else if(number.hxlayer > 0 && number.tlayer == 0) {
+    document.getElementById("number").innerHTML = "E" + commaNumber(Math.round(number.mag)) + "#0#" + (number.pelayer+1) + "#" + (number.hxlayer+1);
+  } else if(number.hxlayer > 0 && number.tlayer > 0) {
+    document.getElementById("number").innerHTML = "E" + Math.pow(10,number.mag%1).toFixed(3) + "E" + commaNumber(Math.floor(number.mag)) + "#" + (number.tlayer-1) + "#" + (number.pelayer+1) + "#" + (number.hxlayer+1);
   }
 };
